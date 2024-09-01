d3e876d00a427cf69ae2015f52f49da305a34f43
Joeken10 <joekentafir@gmail.com>
2024-09-01 20:33:57 +0300

    add files

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# Create an instance of the Book class
+my_book = Book("Caucasian Chalk Circle", 150)
+
+# Access and print the attributes and methods
+print(f"Title: {my_book.title}")
+print(f"Page Count: {my_book.page_count}")
+
+# Call the turn_page method
+my_book.turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+# Create an instance of the Shoe class
+stan_smith= Shoe("Adidas", 9)
+
+# Access and print the attributes and methods
+print(f"Brand: {stan_smith.brand}")
+print(f"Size: {stan_smith.size}")
+print(f"Condition: {stan_smith.condition}")
+
+# Call the cobble method
+stan_smith.cobble()
+
+# Print the updated condition
+print(f"Condition after repair: {stan_smith.condition}")
